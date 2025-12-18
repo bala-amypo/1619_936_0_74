84dc810601a004d557ea35abb2352041e6764ce4
bala-amypo <bala@amypo.in>
2025-12-18 05:16:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
@@ -22,7 +22,7 @@ public class StudentServiceimpl implements StudentService{
         return student.findAll();
     }
     @Override
-    public deleteData(int id){
+    public StudentEntity deleteData(int id){
         student.deleteById(id);
         return "Deleted Successfully";
     }
