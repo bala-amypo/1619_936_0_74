603e4109e2740ec13a64f6f001cfdc2b9b4bb087
bala-amypo <bala@amypo.in>
2025-12-19 04:55:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.example.demo.service.ValidationService;
 import com.example.demo.entity.ValidationEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import jakarta.validation.Valid;
 
 @RestController
