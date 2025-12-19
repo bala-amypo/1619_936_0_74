de565fa0c4e5203598b38ddaeebf8aa9c244f0be
bala-amypo <bala@amypo.in>
2025-12-19 08:20:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/TimeStampController.java
+++ src/main/java/com/example/demo/controller/TimeStampController.java
@@ -1,6 +1,11 @@
 package com.example.demo.controller;
 
-
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.demo.service.ValidationService;
+import com.example.demo.entity.TimeStampEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class TimeStampController{
